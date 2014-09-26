7ed64d94ab0455473e431d7df06d7b94e233394f
Igor Drankin <igor@drankin.ru>
2014-09-26 16:51:27 +0600

    I've finished 3rd part

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "ROR tutorial" }
+
   describe "Home page" do
     it "should have a content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+
+    it "should have a right title like 'BASE_TITLE | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have a content 'Contact Page'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Page')
+    end
+
+    it "should have a right title like BASE_TITLE | Contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 
@@ -16,6 +36,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have a right title like 'BASE_TITLE | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
   end
 
   describe "About page" do
@@ -24,15 +49,11 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-  end
-  
-  describe "Right titles" do
-    it "should have a right title like 'ROR tutorial | Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title('ROR tutorial | Home')
+
+    it "should have a right title like 'BASE_TITLE | About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About")
     end
   end
   
-  
-
 end
