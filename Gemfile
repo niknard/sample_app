8af58f1c6342fd80d07f3a34960df1a29798bfcf
Igor Drankin <igor@drankin.ru>
2014-10-13 19:00:00 +0600

    Added more right spec test for static pages

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+gem 'bootstrap-sass', '2.3.2.0'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '4.0.1'
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,38 +1,42 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+  subject {page}
 
   describe "Home page" do
-    it "should have a content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    before {visit root_path}
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+
   end
 
   describe "Help page" do
-    it "should have a content 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before {visit help_path}
+
+    it { should have_content('Help') }
+    it { should have_title("ROR tutorial") }
+    it { should have_title('| Help') }
+
   end
 
   describe "About page" do
-    it "should have a content 'About Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before {visit about_path}
+
+    it { should have_content('About Us') }
+    it { should have_title("ROR tutorial") }
+    it { should have_title('| About') }
+
   end
-  
-  describe "Right titles" do
-    it "should have a right title like 'ROR tutorial | Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title('ROR tutorial | Home')
-    end
+
+  describe "Contact page" do
+    before {visit contact_path}
+
+    it { should have_content('Contact') }
+    it { should have_title("ROR tutorial") }
+    it { should have_title('| Contact') }
+
   end
-  
-  
 
 end
